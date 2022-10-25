94e5b92d3ea70f9fbb1626f48e1769585cf9013b
ICAN <chicori3@naver.com>
2022-10-25 12:02:43 +0900

    Web3를 이용해 메타마스크 연결 기능 구현

--- pages/vending-machine.js
+++ pages/vending-machine.js
@@ -1,8 +1,26 @@
 import Head from "next/head";
 import 'bulma/css/bulma.css'
 import styles from '../styles/VendingMachine.module.css'
+import Web3 from "web3";
+import { useState } from "react";
 
-export default function VendingMachine() {
+const VendingMachine = () => {
+    let web3
+    const [error, setError] = useState('')
+
+    const connectWalletHandler = async () => {
+        if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
+            try {
+                await window.ethereum.request({ method: "eth_requestAccounts" })
+                web3 = new Web3(window.ethereum)
+            } catch (e) {
+                setError(e.message);
+            }
+        } else {
+            console.log("Please install MetaMask")
+        }
+    }
+
     return (
         <div className={styles.main}>
             <Head>
@@ -15,7 +33,7 @@ export default function VendingMachine() {
                         <h1>Vending Machine</h1>
                     </div>
                     <div className="navbar-end">
-                        <button className="button is-primary">Connect Wallet</button>
+                        <button className="button is-primary" onClick={connectWalletHandler}>Connect Wallet</button>
                     </div>
                 </div>
             </nav>
@@ -24,6 +42,13 @@ export default function VendingMachine() {
                     <p>placeholder text</p>
                 </div>
             </section>
+            <section>
+                <div className="container has-text-danger">
+                    <p>{error}</p>
+                </div>
+            </section>
         </div>
     )
 }
+
+export default VendingMachine;
