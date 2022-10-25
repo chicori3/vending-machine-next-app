799ff0fcba38e9f6517c685bfc32bfbf68a7ef39
ICAN <chicori3@naver.com>
2022-10-25 11:34:48 +0900

    Navbar 구현

---
+++ pages/vending-machine.js
@@ -0,0 +1,29 @@
+import Head from "next/head";
+import 'bulma/css/bulma.css'
+import styles from '../styles/VendingMachine.module.css'
+
+export default function VendingMachine() {
+    return (
+        <div className={styles.main}>
+            <Head>
+                <title>VendingMachine App</title>
+                <meta name="description" content="A blockchain vending app" />
+            </Head>
+            <nav className="navbar mt-4 mb-4">
+                <div className="container">
+                    <div className="navbar-brand">
+                        <h1>Vending Machine</h1>
+                    </div>
+                    <div className="navbar-end">
+                        <button className="button is-primary">Connect Wallet</button>
+                    </div>
+                </div>
+            </nav>
+            <section>
+                <div className="container">
+                    <p>placeholder text</p>
+                </div>
+            </section>
+        </div>
+    )
+}
